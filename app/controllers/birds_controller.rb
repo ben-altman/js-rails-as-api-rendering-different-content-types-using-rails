1bdadccf71a118bd26b745cdd0dc58dad2da3e75
ben-altman <60247694+ben-altman@users.noreply.github.com>
2021-10-09 22:48:15 -0400

    commit tree 5cee840dc23d87f065ef9b3b583824926f8ee1ef

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,13 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render 'birds/index.html.erb'
+    # render 'birds/index.html.erb'
+    # render plain: "Hello #{@birds[3].name}" # outputs string
+    # render json: 'Remember that JSON is just object notation converted to string data, so strings also work here' # outputs string
+    # render json: { message: 'Hashes of data will get converted to JSON' } # outputs hash key = message
+    # render json: ['As','well','as','arrays'] # outputs array
+    # render json: @birds # outputs JS
+    # render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] } # includes more detailed nesting
+    render json: { birds: birds, messages: ['Hello Birds', 'Goodbye Birds'] } # no instance variable necessary
   end
 end
